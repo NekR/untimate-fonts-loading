338e1075e8e97dcc5ff1480ec79ba0c72a3d7de3
Arthur Stolyar <nekr.fabula@gmail.com>
2016-02-08 21:13:37 +0200

    Improve building

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,58 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const del = require('dev');
+const wrap = require('gulp-exports');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const del = require('del');
 
-gulp.task('build', () => {
-  return gulp.src('src/*.js')
+gulp.task('build:node', () => {
+  return gulp.src('src/index.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('lib'));
 });
 
-gulp.task('watch', () => {
-  gulp.watch(['src/*.js'], gulp.series('build'));
+gulp.task('build:browser', () => {
+  return gulp.src('src/index.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(wrap('Fonts'))
+    .pipe(rename('ultimate-fonts-loading.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build:browser-min', () => {
+  return gulp.src('src/index.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(wrap('Fonts'))
+    .pipe(rename('ultimate-fonts-loading.min.js'))
+    .pipe(uglify({
+      compress: {
+        warnings: false,
+        dead_code: true,
+        drop_console: true,
+        unused: true
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', gulp.series('build:node', 'build:browser', 'build:browser'));
+
+gulp.task('watch:node', () => {
+  gulp.watch(['src/*.js'], gulp.series('build:node'));
+});
+
+
+gulp.task('watch:browser', () => {
+  gulp.watch(['src/*.js'], gulp.series('build:browser'));
 });
 
 gulp.task('clean', () => {
   return del(['src/*.js']);
 });
+
